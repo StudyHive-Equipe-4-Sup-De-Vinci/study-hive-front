1065e4c1cdcd46b850400847fa7834f56a9aaf43
Marius <marius.bourse@hotmail.fr>
2025-03-11 14:16:49 +0100

    fix(routeur): change l'exemple du routeur

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -4,7 +4,7 @@ const AppRoot = () => {
   return (
     <Routes>
       <Route path="/" element={<></>} />
-      {/* <Route path="/" element={<Home/>} /> // CECI EST UN EXEMPLE DE COMMENT IMPLEMENTER UNE PAGE*/}
+      {/* <Route path="/profile" element={<Profile/>} /> // CECI EST UN EXEMPLE DE COMMENT IMPLEMENTER UNE PAGE*/}
       <Route path="*" element={<>404</>} />
     </Routes>
   );
