d390ded00e92b03ccd27ad7a708e1f4354d25035
Marius <marius.bourse@hotmail.fr>
2025-03-12 11:21:30 +0100

    fix(merge): fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -12,4 +12,4 @@ export default defineConfig(({ mode }) => {
 
     plugins: [react(), tailwindcss()],
   };
-});;
+});
