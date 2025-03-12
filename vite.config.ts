4003df97b5ab7a7037efb665d38a9a085a4679a2
Marius <marius.bourse@hotmail.fr>
2025-03-12 16:22:27 +0100

    chore(vite): allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
     server: {
       host: "0.0.0.0", // ✅ Permet à Clever Cloud d'écouter sur toutes les interfaces
       port: 8080,
+      allowedHosts: ["app-67d861fe-061f-4753-8e0b-c14b4cdd3973.cleverapps.io"],
     },
   };
 });
